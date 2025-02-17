054d9f9fd1dcf565aef37aab8c7bf08293b39e46
taigawada <private@taigawada.work>
2025-02-17 12:56:54 +0900

    change entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -2,12 +2,12 @@
 echo "current mode is:"
 sqlite3 /root/db/sqlite.db "PRAGMA journal_mode;"
 
+echo "change sqlite journal_mode to:"
+sqlite3 /root/db/sqlite.db "PRAGMA journal_mode=delete;"
+
 mv /root/db/sqlite.db /root/db/tmp_sqlite.db
 cp -p /root/db/tmp_sqlite.db /root/db/sqlite.db
 rm /root/db/tmp_sqlite.db
-
-echo "change sqlite journal_mode to:"
-sqlite3 /root/db/sqlite.db "PRAGMA journal_mode=delete;"
 
 npx prisma migrate deploy
 
