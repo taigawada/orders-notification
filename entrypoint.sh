70391a3db230d69dd130acdbd045521a214dcb64
taigawada <private@taigawada.work>
2025-02-20 21:08:18 +0900

    Isolate entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,17 +1,7 @@
 #!/bin/bash
-echo "current mode is:"
-sqlite3 /root/db/sqlite.db "PRAGMA journal_mode;"
 
-echo "change sqlite journal_mode to:"
-sqlite3 /root/db/sqlite.db "PRAGMA journal_mode=delete;"
-
-mv /root/db/sqlite.db /root/db/tmp_sqlite.db
-cp -p /root/db/tmp_sqlite.db /root/db/sqlite.db
-rm /root/db/tmp_sqlite.db
-
-npx prisma migrate deploy
-
-echo "return sqlite journal_mode to:"
-sqlite3 /root/db/sqlite.db "PRAGMA journal_mode=wal;"
+if [ -e $ENTRYPOINT ]; then
+  bash $ENTRYPOINT
+fi
 
 exec "$@"
